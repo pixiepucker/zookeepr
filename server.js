3007692de01f5c8e7107641f439533a1f70f0dbe
pixiepucker <autumnoffic@gmail.com>
2022-04-11 18:44:47 -0500

    allowed access to different types of data

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 //route to fetch from, then callback w data as parameters
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -53,6 +58,15 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(PORT, ()=>{
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
